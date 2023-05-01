980361930121b52558b49d41120de0c512fe89ce
Anurag Maurya <mauryaanurag375@gmail.com>
2023-05-02 00:29:39 +0530

    commit

--- YACC/lex.yy.c
+++ YACC/lex.yy.c
@@ -284,9 +284,9 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 
 #define YY_NUM_RULES 5
 #define YY_END_OF_BUFFER 6
-static yyconst short int yy_accept[11] =
+static yyconst short int yy_accept[10] =
     {   0,
-        0,    0,    6,    4,    4,    3,    1,    2,    1,    0
+        0,    0,    6,    4,    2,    3,    1,    1,    0
     } ;
 
 static yyconst int yy_ec[256] =
@@ -296,7 +296,7 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    4,    4,    4,
-        4,    4,    4,    4,    4,    4,    4,    1,    5,    1,
+        4,    4,    4,    4,    4,    4,    4,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -321,33 +321,31 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[6] =
+static yyconst int yy_meta[5] =
     {   0,
-        1,    1,    1,    2,    3
+        1,    1,    1,    2
     } ;
 
-static yyconst short int yy_base[13] =
+static yyconst short int yy_base[11] =
     {   0,
-        0,    0,    8,    9,    0,    9,    0,    9,    0,    9,
-        4,    4
+        0,    0,    6,    7,    7,    7,    0,    0,    7,    3
     } ;
 
-static yyconst short int yy_def[13] =
+static yyconst short int yy_def[11] =
     {   0,
-       10,    1,   10,   10,   11,   10,   12,   10,   12,    0,
-       10,   10
+        9,    1,    9,    9,    9,    9,   10,   10,    0,    9
     } ;
 
-static yyconst short int yy_nxt[15] =
+static yyconst short int yy_nxt[12] =
     {   0,
-        4,    5,    6,    7,    4,    9,    8,   10,    3,   10,
-       10,   10,   10,   10
+        4,    5,    6,    7,    8,    9,    3,    9,    9,    9,
+        9
     } ;
 
-static yyconst short int yy_chk[15] =
+static yyconst short int yy_chk[12] =
     {   0,
-        1,    1,    1,    1,    1,   12,   11,    3,   10,   10,
-       10,   10,   10,   10
+        1,    1,    1,    1,   10,    3,    9,    9,    9,    9,
+        9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -367,7 +365,7 @@ char *yytext;
 #include<stdio.h>
 #include "y.tab.h"
 extern int yylval;
-#line 371 "lex.yy.c"
+#line 369 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -520,7 +518,7 @@ YY_DECL
 
 #line 7 "calc.l"
 
-#line 524 "lex.yy.c"
+#line 522 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -571,13 +569,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 11 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 9 );
+		while ( yy_base[yy_current_state] != 7 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -614,7 +612,7 @@ YY_RULE_SETUP
 case 2:
 YY_RULE_SETUP
 #line 12 "calc.l"
-
+;
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
@@ -631,7 +629,7 @@ YY_RULE_SETUP
 #line 15 "calc.l"
 ECHO;
 	YY_BREAK
-#line 635 "lex.yy.c"
+#line 633 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -923,7 +921,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 11 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -958,11 +956,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 11 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 10);
+	yy_is_jam = (yy_current_state == 9);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
